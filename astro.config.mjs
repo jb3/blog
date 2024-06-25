56906d1e284de01169f48b4132d5325526f8b2c4
Joe Banks <joe@jb3.dev>
2024-06-25 20:27:28 +0100

    Enable new plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  vite: {
+    ssr: {
+      noExternal: ['@astrojs/prism']
+    }
+  },
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [rehypeSlug, {}],
+      [rehypeAutolinkHeadings, {
+        behavior: 'append',
+      }],
+      [rehypePrettyCode, {}],
+    ]
+  }
 });
