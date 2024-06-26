1aee6da653f9853c25f02baba2fc90987c6568d9
Joe Banks <joe@jb3.dev>
2024-06-26 04:14:29 +0100

    Add RSS feed

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context: { site: string; }) {
+    const blog = await getCollection('blog');
+    return rss({
+        // `<title>` field in output xml
+        title: "Joe Banks's Blog",
+        // `<description>` field in output xml
+        description: "Various ramblings about topics I find interesting and projects I'm working on",
+        // Pull in your project "site" from the endpoint context
+        // https://docs.astro.build/en/reference/api-reference/#contextsite
+        site: context.site,
+
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.date,
+            description: post.data.blurb,
+            link: `/posts/${post.slug}`
+        }))
+    });
+}
