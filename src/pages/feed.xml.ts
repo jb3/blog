ccc7ea28e883a2270b769ae64acee0c64a32efa6
Joe Banks <joe@jb3.dev>
2024-08-01 22:38:18 +0100

    Add self atom:link to generated RSS feed

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -8,10 +8,13 @@ export async function GET(context: { site: string; }) {
         title: "Joe Banks's Blog",
         // `<description>` field in output xml
         description: "Various ramblings about topics I find interesting and projects I'm working on",
+        xmlns: {
+            'atom': 'http://www.w3.org/2005/Atom'
+        },
         // Pull in your project "site" from the endpoint context
         // https://docs.astro.build/en/reference/api-reference/#contextsite
         site: context.site,
-
+        customData: `<atom:link href="${import.meta.env.SITE}/feed.xml" rel="self" type="application/rss+xml" />`,
         items: blog.sort((a, b) => {
             return new Date(b.data.date).getTime() - new Date(a.data.date).getTime();
         }).map((post) => ({
