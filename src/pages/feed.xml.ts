dde1470522718e77dbe97c4fbd6d7e03131ec865
Joe Banks <joe@jb3.dev>
2025-10-20 21:02:16 +0100

    Use new RSS stylesheet when generating feed.xml

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -4,15 +4,12 @@ import { getCollection } from 'astro:content';
 export async function GET(context: { site: string; }) {
     const blog = await getCollection('blog');
     return rss({
-        // `<title>` field in output xml
         title: "Joe Banks's Blog",
-        // `<description>` field in output xml
         description: "Various ramblings about topics I find interesting and projects I'm working on",
         xmlns: {
             'atom': 'http://www.w3.org/2005/Atom'
         },
-        // Pull in your project "site" from the endpoint context
-        // https://docs.astro.build/en/reference/api-reference/#contextsite
+        stylesheet: '/rss.xsl',
         site: context.site,
         customData: `<atom:link href="${import.meta.env.SITE}/feed.xml" rel="self" type="application/rss+xml" />`,
         items: blog.sort((a, b) => {
