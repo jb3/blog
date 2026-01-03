318648104d436e21b9d838a35a7caa8c8ebf4644
Joe Banks <joe@jb3.dev>
2026-01-03 17:42:12 +0000

    Dynamic favicons & icons with webmanifest from astro-webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkToc from 'remark-toc';
 import embeds from "astro-embed/integration";
 import expressiveCode from "astro-expressive-code";
+import webmanifest from "astro-webmanifest";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -14,6 +15,18 @@ export default defineConfig({
     LinkPreview: false
   }), mdx(), sitemap({
     filter: (page) => !page.includes('/projects/jeremiah-boby')
+  }), webmanifest({
+    name: "Joe Banks",
+    icon: "./src/images/icon.jpg",
+    short_name: "Joe Banks",
+    description: "The personal blog of Joe Banks",
+    start_url: "/",
+    background_color: "#ffffff",
+    theme_color: "#616283",
+    display: "standalone",
+    config: {
+      insertAppleTouchLinks: true,
+    }
   })],
   markdown: {
     syntaxHighlight: false,
